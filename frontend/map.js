7f4cdec1202f3ca5b57fa00f233dbaa53ab8902a
Emma-He <emma.xiaotong@gmail.com>
2021-01-09 17:22:37 -0400

    added radius function

--- frontend/map.js
+++ frontend/map.js
@@ -23,7 +23,6 @@ const control = document.getElementById("floating-panel");
 control.style.display = "block";
 map.controls[google.maps.ControlPosition.TOP_CENTER].push(control);
 
-
 //define calcRoute function
 function calcRoute() {
     //create request
@@ -39,10 +38,9 @@ function calcRoute() {
         if (status == google.maps.DirectionsStatus.OK) {
             //Get distance and time
             $("#output").html("<div class='alert-info'>From: " + document.getElementById("from").value +
-                ".<br />To: " + document.getElementById("to").value +
+                ".<br />To: " + document.getElementById("to").value, +
                 ".<br /> Driving distance: " + result.routes[0].legs[0].distance.text +
                 ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
-
             //display route
             directionsDisplay.setDirections(result);
         } else {
@@ -54,7 +52,47 @@ function calcRoute() {
             $("#output").html("<div class='alert-danger'>Could not retrieve driving distance.</div>");
         }
     });
+}
 
+//create a list of donee locations
+let destinationList = ["Stockholm, Sweden", "Sollentuna, Sweden", "Galärvarvsvägen 14, 115 21 Stockholm, Sweden"];
+
+//create an empty list of distances
+let distanceList = [];
+
+//calculate distance between origin donee and other donees around it
+function calcDistance() {
+    //loop through all the donees
+    for (let i = 0; i < destinationList.length; i++) {
+        //create request
+        var request = {
+                origin: document.getElementById("from").value,
+                destination: destinationList[i],
+                travelMode: google.maps.TravelMode.DRIVING,
+                unitSystem: google.maps.UnitSystem.METRIC
+            }
+            //pass the request to the route method
+        directionsService.route(request, function(result, status) {
+            if (status == google.maps.DirectionsStatus.OK) {
+                distanceList[i] = result.routes[0].legs[0].distance;
+            } else {
+                //show error message
+                $("#output").html("<div class='alert-danger'>Could not retrieve driving distance.</div>");
+            }
+        });
+    }
+}
+
+//create a new array of donees within the 50 km radius 
+let newDistanceList = [];
+
+//create an array of donees within the 50 km radius from the origin donee
+function restrictDistance() {
+    for (let i = 0; i < distanceList.length; i++) {
+        if (distanceList[i].value <= 50000) {
+            newDistanceList.push(distanceList[i]);
+        }
+    }
 }
 
 //create autocomplete objects for all inputs
