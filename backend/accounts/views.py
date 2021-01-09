0ad55dfbe8f6ed058aa091f79d9c4e519c4e4ec7
Danielle <xdanielle.dsouza@gmail.com>
2021-01-09 14:28:12 -0500

    added user models and sign in/sign up

---
+++ backend/accounts/views.py
@@ -0,0 +1,41 @@
+from django.contrib.auth.models import User
+from rest_framework.views import APIView
+from rest_framework import permissions
+from rest_framework.response import Response
+from user_profile.models import UserProfile
+
+# Create your views here.
+class SignUpView(APIView):
+    permission_classes = (permissions.AllowAny, )
+
+    def post(self, request, format=None):
+        data = self.request.data
+
+        username = data['username']
+        password = data['password']
+        verify_password = data['verify_password']
+        group_name = data['group_name']
+        phone = data['phone']
+        address = data['address']
+        user_type = data['user_type']
+
+        if password == verify_password:
+            if User.objects.filter(username=username).exists():
+                return Response({'error': "Username already exists"})
+            else:
+                if len(password) < 8:
+                    return Response({'error': 'Password must be at least 8 characters'})
+                else:
+                    # Create user
+                    user = User.objects.create_user(username=username, password=password)
+                    user.save()
+
+                    user = User.objects.get(username=username)
+
+                    # Create user profile
+                    user_profile = UserProfile(user=user, group_name=group_name, phone=phone, address=address, user_type=user_type)
+                    user_profile.save()
+
+                    return Response({'success': 'User account created'})
+        else:
+            return Response({ 'error': 'Passwords do not match' })
--- backend/bridgeit/urls.py
+++ backend/bridgeit/urls.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    path('api/v1/api-auth', include('rest_framework.urls')),
+    path('api/v1/token', TokenObtainPairView.as_view()),
+    path('api/v1//refresh', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
     path('api/v1/', include('darkspots.urls')),
     path('api/v1/', include('geotabs.urls')),
     path('api/v1/', include('mobility.urls')),
-    path('api/v1/items/', include('items.urls'))
+    path('api/vi/accounts/', include('accounts.urls')),
+    path('api/vi/profile/', include('user_profile.urls')),
+    path('api/v1/items/', include('items.urls')),
 ]
