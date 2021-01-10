6a3219a1f1c57f406471e5dbd0b95edc9e83e0c9
Danielle <xdanielle.dsouza@gmail.com>
2021-01-10 00:27:28 -0500

    Added loader

---
+++ frontend/src/components/UI/Loader.js
@@ -0,0 +1,44 @@
+import styled, { keyframes } from "styled-components";
+
+const Loader = () => {
+  return (
+    <LoaderWrapper>
+        <MainLoader />
+    </LoaderWrapper>
+  );
+};
+
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoaderWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const MainLoader = styled.div`
+  animation: ${rotate360} 1s linear infinite;
+  transform: translateZ(0);
+  
+  background: transparent;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  ${({ theme }) => `
+    border-top: 2px solid ${theme.colors.secondary};
+    border-right: 2px solid ${theme.colors.secondary};
+    border-bottom: 2px solid ${theme.colors.secondary};
+    border-left: 4px solid ${theme.colors.primary};
+  `}
+`;
+
+export default Loader;
