e11fbdbd2812177a1b68311a36e30cc22e47b840
Danielle <xdanielle.dsouza@gmail.com>
2021-01-09 20:36:16 -0500

    Update to use CSRF cookies

--- backend/user_profile/urls.py
+++ backend/user_profile/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GetUserProfileView
+from .views import GetUserProfileView, UpdateUserProfileView
 
 urlpatterns = [
-    path('user', GetUserProfileView.as_view())
+    path('user', GetUserProfileView.as_view()),
+    path('update', UpdateUserProfileView.as_view()),
 ]
